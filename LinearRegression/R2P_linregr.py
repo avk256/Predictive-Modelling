9cfcd63c5062467ec300c38a88d5d409a4ee1754
avk256 <avk256@gmail.com>
2019-07-12 14:25:12 +0300

    Edited First commit

--- LinearRegression/R2P_linregr.py
+++ LinearRegression/R2P_linregr.py
@@ -1,14 +1,41 @@
 import sys
+### change to the current script folder
 sys.path.insert(0, 'C:\\Users\Alex\\Google Drive')
 import h2o
 import pandas as pd
 import json
 import R2P_linregr_dataprep
-from R2P_linregr_dataprep import columns_data_type, ch_sq_test, correlations, transformation, variable_importance_h2o 
 import seaborn as sns
 import matplotlib.pyplot as plt
 from h2o.estimators.glm import H2OGeneralizedLinearEstimator
 from h2o.estimators.gbm import H2OGradientBoostingEstimator
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+from R2P_linregr_dataprep import columns_data_type, ch_sq_test, correlations, transformation, variable_importance_h2o, transformation_inv 
+
+############################################################################
+
+############################################################################
+
+
+"""
+ Variance Inflation Factor (VIF) Explained method implements 
+ variance_inflation_factor from statsmodels  
+ 
+ input:
+     daframe with Independent variables
+     VIF threshold value
+ output:
+     List of multicollinear variables
+"""
+def vif(num_data, y, thresh):
+    num_data = num_data.drop([y], axis=1)
+    vif = pd.DataFrame()
+    vif["VIF Factor"] = [variance_inflation_factor(num_data.values, i) for i in range(num_data.shape[1])]
+    vif["features"] = num_data.columns
+    return list(vif["features"][vif["VIF Factor"]>thresh])
+
+    
 
 """
  Linear regression method implements 
@@ -26,23 +53,30 @@ from h2o.estimators.gbm import H2OGradientBoostingEstimator
     #* @param parametersObj
     #* @param columnsArray
  output:
-     linear_regression
+     R square value,
+     Test data with predictions
+     RMSE
+     Model coefficients
+     Correlation matrix
+     list of variable importance
+     list of multicollinear variables
 """
 def linear_regression(dbHost="",dbPort="",userName="",password="",dbName="",query="",yValue="",xValues="",parametersObj="",columnsArray=""):
     ##############
     # connecting to BD
     ##############
 
-    data = pd.read_csv("E:/Work/aspirantura/kafedra/upwork/RtoP/Models_sep/Datasets/50000_Sales_Records_Dataset .csv")
-    
+    data = pd.read_csv("E:/Work/aspirantura/kafedra/upwork/RtoP/Models_sep/Datasets/50000_Sales_Records_Dataset_e.csv")
     data = columns_data_type(data, columnsArray)
-    ent_cor,chisq_dependency = correlations(data)
-    print(ent_cor,chisq_dependency)
-    data = transformation(data)
+    ent_cor,chisq_dependency,data,rm_cols, miss_cols, obj_t = correlations(data)
+    #print(ent_cor,chisq_dependency, rm_cols, miss_cols)
     
     num_data = data.select_dtypes(include=['number']).copy()
     variable_imp = variable_importance_h2o(data, list(num_data.columns), yValue)
+    vif_var = vif(num_data, yValue, 10)
+    #print(variable_imp, vif_var)
 
+    
     if(data[yValue].dtypes == 'float') or (data[yValue].dtypes == 'int'):
         print("Finding variable importance by taking given numeric variable as a dependent variable")
         hf = h2o.H2OFrame(num_data)
@@ -50,13 +84,17 @@ def linear_regression(dbHost="",dbPort="",userName="",password="",dbName="",quer
         train, valid, test = hf.split_frame(ratios=[.8, .1])
         
         glm_model = H2OGeneralizedLinearEstimator(family = 'gaussian')
-#        print(glm_model)
         glm_model.train(xValues, yValue, training_frame= train, validation_frame=valid)
-#        print(glm_model)
+        print(glm_model)
         predicted = glm_model.predict(test_data=test)
         
-
-        linear_regr = [predicted, test[yValue],glm_model.r2(),variable_imp]
+        test_inv = transformation_inv(test.as_data_frame(),obj_t)
+        true_y = test_inv[yValue]
+        test[yValue] =  predicted
+        test_inv = transformation_inv(test.as_data_frame(),obj_t)
+        pred_y = test_inv[yValue]
+        
+        linear_regr = [pred_y, true_y ,glm_model.r2(),glm_model.rmse(),ent_cor,glm_model.coef(),variable_imp,vif_var]
         
     else:
         print("Finding variable importance by taking categorical variables as dependent variable")
@@ -64,11 +102,31 @@ def linear_regression(dbHost="",dbPort="",userName="",password="",dbName="",quer
         gbm.train(xValues, yValue, training_frame= train, validation_frame=valid)
 #        print(gbm)
         predicted2 = gbm.predict(test_data=test)
-        linear_regr = [predicted2, test[yValue], gbm.r2(),variable_imp]
+
+        test_inv = transformation_inv(test.as_data_frame(),obj_t)
+        true_y = test_inv[yValue]
+        test[yValue] =  predicted2
+        test_inv = transformation_inv(test.as_data_frame(),obj_t)
+        pred2_y = test_inv[yValue]
+
+
+        linear_regr = [pred2_y, true_y ,glm_model.r2(),glm_model.rmse(),ent_cor,glm_model.coef(),variable_imp,vif_var]
 
     
     return linear_regr
 
+"""
+ Generate text description of the data and model 
+ 
+ input:
+     linear_regr - data structure with results of linear regression model
+ output:
+     lists with text description 
+"""
+
+def NLG(linear_regr):
+    pass
+    
 """
  Plotting method 
  
@@ -78,12 +136,14 @@ def linear_regression(dbHost="",dbPort="",userName="",password="",dbName="",quer
      plots: R^2, Predicted-Actual Value, Variable importance
 """
 
-def NLG(linear_regr):
-    pred_val = linear_regr[0].as_data_frame()
-    act_val = linear_regr[1].as_data_frame()
+def plotting(linear_regr):
+ 
+    pred_val = linear_regr[0]
+    act_val = linear_regr[1]
+    
     r2 = linear_regr[2]
-    df = pd.concat([pred_val,act_val], axis=1)
-    var_imp = pd.DataFrame(linear_regr[3][0])
+    
+    var_imp = pd.DataFrame(linear_regr[6][0])
     var_imp.columns = ['variable', 'relative_importance', 'scaled_importance', 'percentage']
     
     sns.distplot(var_imp['relative_importance'],kde = False, axlabel = list(var_imp['variable']))
@@ -95,13 +155,24 @@ def NLG(linear_regr):
     ax1.set_ylabel('Actual value')
     print("Linear regression accuracy: R^2")
     print(r2)
+
+
     
 ############## testing linear regression
 
 ### example of columnsArray
-columnsArray = '[{"columnDisplayName":"Sno","tableDisplayType":"string","columnName":"Sno"},{"columnDisplayName":"Region","tableDisplayType":"string","columnName":"Region"},{"columnDisplayName":"Country","tableDisplayType":"string","columnName":"Country"},{"columnDisplayName":"Item Type","tableDisplayType":"string","columnName":"Item Type"},{"columnDisplayName":"Sales Channel","tableDisplayType":"string","columnName":"Sales Channel"},{"columnDisplayName":"Order Priority","tableDisplayType":"string","columnName":"Order Priority"},{"columnDisplayName":"Order Date","tableDisplayType":"string","columnName":"Order Date"},{"columnDisplayName":"Order ID","tableDisplayType":"string","columnName":"Order ID"},{"columnDisplayName":"Ship Date","tableDisplayType":"string","columnName":"Ship Date"},{"columnDisplayName":"Units Sold","tableDisplayType":"number","columnName":"Units Sold"},{"columnDisplayName":"Unit Price","tableDisplayType":"number","columnName":"Unit Price"},{"columnDisplayName":"Unit Cost","tableDisplayType":"number","columnName":"Unit Cost"},{"columnDisplayName":"Total Revenue","tableDisplayType":"number","columnName":"Total Revenue"},{"columnDisplayName":"Total Cost","tableDisplayType":"number","columnName":"Total Cost"},{"columnDisplayName":"target_var","tableDisplayType":"number","columnName":"target_var"}]'
-columnsArray = json.loads(columnsArray)
-columnsArray = pd.DataFrame(columnsArray)
 
-linear_regr = linear_regression(yValue='target_var',xValues=['Total Revenue'],columnsArray=columnsArray)
+columnsArray = '[{"columnDisplayName":"S@#@#no","tableDisplayType":"string","columnName":"S#@#@no"},{"columnDisplayName":"Region","tableDisplayType":"string","columnName":"Region"},{"columnDisplayName":"Country","tableDisplayType":"string","columnName":"Country"},{"columnDisplayName":"Item Type","tableDisplayType":"string","columnName":"Item Type"},{"columnDisplayName":"Sales Channel","tableDisplayType":"string","columnName":"Sales Channel"},{"columnDisplayName":"Order Priority","tableDisplayType":"string","columnName":"Order Priority"},{"columnDisplayName":"Order Date","tableDisplayType":"string","columnName":"Order Date"},{"columnDisplayName":"Order ID","tableDisplayType":"string","columnName":"Order ID"},{"columnDisplayName":"Ship Date","tableDisplayType":"string","columnName":"Ship Date"},{"columnDisplayName":"Units Sold","tableDisplayType":"number","columnName":"Units Sold"},{"columnDisplayName":"Unit Price","tableDisplayType":"number","columnName":"Unit Price"},{"columnDisplayName":"Unit Cost","tableDisplayType":"number","columnName":"Unit Cost"},{"columnDisplayName":"Total Revenue","tableDisplayType":"number","columnName":"Total Revenue"},{"columnDisplayName":"Total Cost","tableDisplayType":"number","columnName":"Total Cost"},{"columnDisplayName":"target_var","tableDisplayType":"number","columnName":"target_var"}]'    
+#columnsArray = '[{"columnDisplayName":"Region","tableDisplayType":"string","columnName":"Region"},{"columnDisplayName":"Country","tableDisplayType":"string","columnName":"Country"},{"columnDisplayName":"Item Type","tableDisplayType":"string","columnName":"Item Type"},{"columnDisplayName":"Sales Channel","tableDisplayType":"string","columnName":"Sales Channel"},{"columnDisplayName":"Order Priority","tableDisplayType":"string","columnName":"Order Priority"},{"columnDisplayName":"Order Date","tableDisplayType":"string","columnName":"Order Date"},{"columnDisplayName":"Order ID","tableDisplayType":"string","columnName":"Order ID"},{"columnDisplayName":"Ship Date","tableDisplayType":"string","columnName":"Ship Date"},{"columnDisplayName":"Units Sold","tableDisplayType":"number","columnName":"Units Sold"},{"columnDisplayName":"Unit Price","tableDisplayType":"number","columnName":"Unit Price"},{"columnDisplayName":"Unit Cost","tableDisplayType":"number","columnName":"Unit Cost"},{"columnDisplayName":"Total Revenue","tableDisplayType":"number","columnName":"Total Revenue"},{"columnDisplayName":"Total Cost","tableDisplayType":"number","columnName":"Total Cost"},{"columnDisplayName":"target_var","tableDisplayType":"number","columnName":"target_var"}]'    
+try:
+    columnsArray = json.loads(columnsArray)
+except:
+    print('json format not valid')
+
+
+
+linear_regr = linear_regression(yValue='Units Sold',xValues=['Total Revenue'],columnsArray=columnsArray)
+
+plotting(linear_regr)
+
 NLG(linear_regr)
--- LinearRegression/R2P_linregr_dataprep.py
+++ LinearRegression/R2P_linregr_dataprep.py
@@ -4,7 +4,7 @@ from h2o.estimators.glm import H2OGeneralizedLinearEstimator
 from h2o.estimators.gbm import H2OGradientBoostingEstimator
 
 import numpy as np
-import pandas as pd
+import pandas as pd # must be 0.24.0
 import sklearn
 
 from pandas.io.json import json_normalize
@@ -13,10 +13,16 @@ from scipy.stats import chi2
 import itertools
 from sklearn.preprocessing import PowerTransformer
 from sklearn.preprocessing import LabelEncoder
+import statistics
+####################################################################
+
+####################################################################
+
 
 """
- change names of the featrures
- set types of features: numeric or categorical
+ Changing names of the featrures.
+ Set types of features: numeric or categorical.
+ Replacing special characters in column names.
  input:
      df - data frame
      columnsArray - data frame with names and types of columns
@@ -25,16 +31,101 @@ from sklearn.preprocessing import LabelEncoder
 """
 def columns_data_type(df, columnsArray = ""):
     columnsArray = pd.DataFrame(columnsArray)
-    df = df.dropna()
+    columnsArray = columnsArray.replace(r'[^a-zA-Z0-9 -]', "", regex=True)
     df.columns = columnsArray['columnName'] 
+    df = df.replace(r'^\s*$', np.nan, regex=True)
+
     for i in columnsArray['columnName']:
         sub = columnsArray[columnsArray['columnName'] == i]
         if (sub['tableDisplayType'].values[0] == 'number'):
             df[i] = pd.to_numeric(df[i].values)
         if (sub['tableDisplayType'].values[0] == 'string'):
+            #df[i] = str(df[i])
             df[i] = df[i].astype('category')
             
     return df
+
+"""
+ Imputation or removing of missing values using mean an mode.
+ Replacing blanks with NA's.
+ input:
+     df - data frame
+     method
+         drop - drop all NA's
+         mean - mean for numeric and 
+         mode - for categorical
+ output:
+     df - changed data frame
+"""
+def missing_val_impute(df, method):
+    try:
+
+        miss_count = pd.DataFrame(df.isna().sum())
+        miss_count.columns = ['miss_count']
+        
+        cat_data = df.select_dtypes(include=['category']).copy()
+        num_data = df.select_dtypes(include=['number']).copy()
+
+        if (method == 'drop'):
+            df = df.dropna()
+        elif (method == 'impute'):
+            num_data = num_data.fillna(num_data.mean())
+            for i in list(cat_data.columns):
+                cat_data[i] = cat_data[i].fillna((cat_data[i].mode(dropna=True))[0])
+                    
+        else:
+            print("Imputation method not specify")        
+        frames = [cat_data,num_data]
+        df = pd.concat(frames, axis=1)
+    except:
+        print("Imputation method doesn't meet the data")
+        df = df.dropna()
+    return df, miss_count
+
+"""
+ Removing columns that contains huge number of levels
+ Removing zero variance column
+ input:
+     df - data frame
+     ratio - ratio observations to levels
+ output:
+     df - changed data frame
+     removed_cols - list of removed columns
+"""
+def remove_col(df, ratio):
+    try:
+        cat_data = df.select_dtypes(include=['category']).copy()
+        num_data = df.select_dtypes(include=['number']).copy()
+
+        num_level_cat = []
+        removed_cols = []
+        for i in list(cat_data.columns):
+            cat_list = list(cat_data[i].unique())
+            num_obs = cat_data[i].count() 
+            for j in cat_list:
+                num_level_cat.append([i,j,cat_data[i][cat_data[i]== j].count(),num_obs])
+        num_level_cat = pd.DataFrame(num_level_cat)
+        num_level_cat.columns = ['category','level','count_level','count_observ']
+        
+        for i in list(num_level_cat['category'].unique()):
+            if (len(cat_data) / num_level_cat['level'][num_level_cat['category']==i].count() < ratio):
+                cat_data = cat_data.drop(i, 1)
+                removed_cols.append(i)
+#Removing zero variance column
+        var = pd.DataFrame(num_data.var())
+        for i in list(var.index):
+            if list(var[var.index==i][0])[0] == 0:
+                num_data = num_data.drop(i, 1)
+                removed_cols.append(i)
+        frames = [cat_data,num_data]
+        transformed_data = pd.concat(frames, axis=1)
+           
+    except:
+        print("Exception in removing columns")
+    return transformed_data,removed_cols
+
+
+
 
 """
  chi square test for correlation between categorical features
@@ -52,59 +143,101 @@ def ch_sq_test(cat_data):
         combos = pd.DataFrame(combos)
         ind1 = list(combos[0])
         ind2 = list(combos[1])
-
+        print(list(cat_data[cat_data.columns[ind1[1]]]))
         for i in range(len(ind1)):
-            
+            print(i)
+
             try:
                 test.append(chi2_contingency([list(cat_data[cat_data.columns[ind1[i]]]),list(cat_data[cat_data.columns[ind2[i]]])]))
             except:
                 continue
         test = pd.DataFrame(test)
-        test.columns = ['stat', 'p-val', 'dof', 'expected']
+        #test.columns = ['stat', 'p-val', 'dof', 'expected']
         return test    
     elif ncol == 1:
         print("There is only one category field exists")
     else:
         print("No category field exists")
-        
-    
-"""
- correlations in the data
- Pearson test for numerical
- chi squared test for categorical
- input:
-     df - data frame 
- output:
-     corr matrix between numeric features
-     chi squared method result for categorical features
-"""
-
-def correlations(data):
-    cat_data = data.select_dtypes(include=['category']).copy()
-    num_data = data.select_dtypes(include=['number']).copy()
-    ent_cor = num_data.corr()
-    chisq_dependency = ch_sq_test(cat_data)
-    return ent_cor,chisq_dependency 
+     
 
 """
- transformation method implements 
+ Transformation method implements 
  YeoJohnson transformation for numeric features 
  (power transform featurewise to make data more Gaussian-like)
  input:
      df - data frame 
  output:
      transformed data frame
+     PowerTransformer() object for invert transformation
 """
 def transformation(data):
     cat_data = data.select_dtypes(include=['category']).copy()
     num_data = data.select_dtypes(include=['number']).copy()
     pt = PowerTransformer() 
+
     transformed = pt.fit(num_data).transform(num_data)
     transformed = pd.DataFrame(transformed)
     transformed.columns = num_data.columns
+
     frames = [cat_data,transformed]
     transformed_data = pd.concat(frames, axis=1)
-    return transformed_data     
+    return transformed_data, pt     
+
+"""
+ Transformation invert method get the origin values back after
+ YeoJohnson transformation
+ input:
+     df - data frame 
+ output:
+     original values data frame
+"""
+def transformation_inv(data, obj):
+#    cat_data = data.select_dtypes(include=['category']).copy()
+#    num_data = data.select_dtypes(include=['number']).copy() 
+    num_data = data
+    transformed = obj.inverse_transform(num_data)
+    transformed = pd.DataFrame(transformed)
+    transformed.columns = num_data.columns
+
+#    frames = [cat_data,transformed]
+#    transformed_data = pd.concat(frames, axis=1)
+    return transformed
+
+
+"""
+ Correlations in the data
+ Pearson test for numerical
+ Chi squared test for categorical
+ input:
+     data - data frame outputed be columns_data_type() 
+ output:
+     corr matrix between numeric features
+     chi squared method result for categorical features
+     preprocessed data
+     list of exclude columns
+     list of missing values amount for each columns 
+"""
+def correlations(data):
+    # data type conversion and deleting missing values 
+    data, miss_cols = missing_val_impute(data, method='impute')
+    data, rm_cols = remove_col(data, ratio=3)
+    data, obj_t = transformation(data)
+    cat_data = data.select_dtypes(include=['category']).copy()
+    num_data = data.select_dtypes(include=['number']).copy()
+    if (len(num_data.columns)>1):
+        ent_cor = num_data.corr()
+    else:
+        print("There is only one feature exists. You need at least two to analyse")
+    if (len(cat_data.columns)>1):
+        chisq_dependency = ch_sq_test(cat_data)
+    else:
+        print("There is only one feature exists. You need at least two to analyse")
+#    frames = [cat_data, num_data]
+#    data = pd.concat(frames, axis=1)
+        
+    return ent_cor,chisq_dependency,data,rm_cols, miss_cols, obj_t  
+
+
 
 """
  Determine variable importance method implements 
